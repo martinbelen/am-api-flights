bce4cd6a4d0b49b31a5a0bbc423cdc0af99cffc0
Martin Belen <martin.belen@almundo.com>
2016-10-25 17:29:53 -0300

    test script

---
+++ test/flight.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const FlightService = require('../lib/flight.service');
+
+describe('Fligth',() => {
+	describe('getFlights()', () => {
+		it('should return a valid request with status 200', (done) => {
+			FlightService.getFlights()
+			.then((response) => {
+				assert.ok(response);
+			})
+			.catch((error) => {
+				done(error);
+			});
+		});
+	});
+});
